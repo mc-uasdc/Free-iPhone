e55fefddb8280f14aa4d162de623ac99a23fb74f
mc-uasdc <58667084+mc-uasdc@users.noreply.github.com>
2019-12-08 14:30:42 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".bep").click(function() {
+    $(".bep").attr("src", "http://www.leseriail.com/cdn/3/2011/839/credit-card-payment-authorization-form-template_262986.jpg");
+ $(".zep").attr("src", "https://sophosnews.files.wordpress.com/2018/08/pronblock-1200.png?w=780&h=408&crop=1");
+});
+    
+    
+    
+    
+    
+    
+    
